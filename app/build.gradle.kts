c4daff184b3e2022fd8d0228d136abf313e3bbc2
SharonWendoh <swendoh.sharon@gmail.com>
2023-11-30 16:52:55 +0300

    Fixed Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+    //id ("dagger.hilt.android.plugin")
 }
 
 android {
@@ -40,7 +41,13 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Dependency injection
+    /*implementation ("com.google.dagger:hilt-android:2.48.1")
+    annotationProcessor("com.google.dagger:hilt-android-compiler:2.48.1")*/
+
 }
